bca1eef3def241790faf27216ee90372bb8b620f
triaksh25 <100975695+triaksh25@users.noreply.github.com>
2023-03-10 11:01:35 -0700

    Test #2 will fail

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,9 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -12,10 +15,13 @@ import java.util.ArrayList;
 
 public class CustomListTest {
     private CustomList list;
-    
+
     public CustomList MockCityList(){
         list = new CustomList(null,new ArrayList<>());
         return list;
+    }
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
     }
 
     @Test
@@ -24,6 +30,16 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
+    }
+
+    @Test
+    public void testHasCity() {
+        list = MockCityList();
+        City city = mockCity();
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+        City city2 = new City("Charlottetown", "Prince Edward Island");
+        assertFalse(list.hasCity(city2));
     }
 
 }
